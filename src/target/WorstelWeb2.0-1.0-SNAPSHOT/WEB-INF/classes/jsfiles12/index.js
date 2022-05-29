4ac6aa5ab1d7cd2a28f58a6b07cc3c92acbdd094
pascal134 <55754844+pascal134@users.noreply.github.com>
2022-05-29 13:15:12 +0200

    Add files via upload

---
+++ src/target/WorstelWeb2.0-1.0-SNAPSHOT/WEB-INF/classes/jsfiles12/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+    add,
+    subtract,
+    divide,
+    multiply
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}

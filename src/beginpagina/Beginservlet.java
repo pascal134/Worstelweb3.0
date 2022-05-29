86ddb09dd49c5ecf52bd2e629e176f2dba642847
pascal134 <55754844+pascal134@users.noreply.github.com>
2022-05-29 13:13:09 +0200

    Add files via upload

---
+++ src/beginpagina/Beginservlet.java
@@ -0,0 +1,12 @@
+package beginpagina;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns ="/beginpagina.html")
+public class Beginservlet extends HttpServlet{
+
+    }
